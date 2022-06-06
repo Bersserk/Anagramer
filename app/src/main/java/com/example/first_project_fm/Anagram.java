a73fcc87ca00b4a9096af0f44036fc6627d53fc3
Bersserk <bers.developer@gmail.com>
2022-06-06 21:48:36 +0200

    1. Added the new Anagram class with pattern for methods 2. Added in Main class some methods 3. added the necessary views

---
+++ app/src/main/java/com/example/first_project_fm/Anagram.java
@@ -0,0 +1,11 @@
+package com.example.first_project_fm;
+
+public class Anagram {
+    Anagram() {
+        buildAnagram();
+    }
+
+    static void buildAnagram (){
+        // logic for build Anagram
+    }
+}
--- app/src/main/java/com/example/first_project_fm/MainActivity.java
+++ app/src/main/java/com/example/first_project_fm/MainActivity.java
@@ -1,14 +1,33 @@
-package com.example.first_project_fm;
+    package com.example.first_project_fm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.inputText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView textView = findViewById(R.id.anagramText);
+                textView.setText(s);
+            }
+        });
     }
 }
