b093bf4cee16b507f963c2dc4d6deb519e8ce66e
Bersserk <bers.developer@gmail.com>
2022-06-07 21:01:25 +0200

    1. Added files into gitignore

--- app/src/main/java/com/example/first_project_fm/Anagram.java
+++ app/src/main/java/com/example/first_project_fm/Anagram.java
@@ -1,10 +1,7 @@
 package com.example.first_project_fm;
 
 public class Anagram {
-    Anagram() {
-        buildAnagram();
-    }
-
+    
     static void buildAnagram (){
         // logic for build Anagram
     }
