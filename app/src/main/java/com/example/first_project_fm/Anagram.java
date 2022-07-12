4a2ba7a649b9f5d59009bbdb495aa075b5e74257
Bers <bers.developer@gmail.com>
2022-07-12 21:54:35 +0200

    1. changed logical for one loop and simply

--- app/src/main/java/com/example/first_project_fm/Anagram.java
+++ app/src/main/java/com/example/first_project_fm/Anagram.java
@@ -24,51 +24,31 @@ public class Anagram {
     }
 
     private static String buildAnagramOfWord(String word, String mFilter) {
+        StringBuilder str = new StringBuilder(word);
 
-        for (int i = 0, k = word.length() - 1; i < k; i++, k--) {
-            switch (moveOrNot(word.charAt(i), word.charAt(k), mFilter)) {
-                case 1:
-                    while (String.valueOf(word.charAt(i)).matches(mFilter)) {
-                        i++;
-                    }
-                case 2:
-                    while (String.valueOf(word.charAt(k)).matches(mFilter)) {
-                        k--;
-                    }
-                case 4:
-                    word = changeChars(i, k, word);
-                    break;
-                case 3:
+        for (int i = 0, k = word.length() - 1; i < k; ) {
+            if (check(word.charAt(i), mFilter)) {
+                if (check(word.charAt(k), mFilter)) {
+                    char tmp = word.charAt(k);
+                    str.deleteCharAt(k);
+                    str.insert(k, word.charAt(i));
+                    str.deleteCharAt(i);
+                    str.insert(i, tmp);
                     i++;
                     k--;
-                    break;
+                } else {
+                    k--;
+                }
+            } else {
+                i++;
             }
         }
-        return word + " ";
+        return str + " ";
     }
 
-    // filtering method. Returns '1' if the first letter filters, '2' if the second, '4' if none and '3' if both
-    private static int moveOrNot(char first, char second, String filter) {
-        boolean result1 = String.valueOf(first).matches(filter);
-        boolean result2 = String.valueOf(second).matches(filter);
-        if (result1 && result2) {
-            return 3;
-        } else if (result1) {
-            return 1;
-        } else if (result2) {
-            return 2;
-        } else {
-            return 4;
-        }
-    }
-
-    // method for change words
-    private static String changeChars(int index1, int index2, String tempIn) {
-        StringBuilder tempV = new StringBuilder(tempIn);
-        char temp = tempV.charAt(index1);
-        tempV.setCharAt(index1, tempV.charAt(index2));
-        tempV.setCharAt(index2, temp);
-        return tempV.toString();
+    private static boolean check(char s, String filter) {
+        String st = String.valueOf(s);
+        return !st.matches(filter);
     }
 
 }
