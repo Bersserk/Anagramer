d30cf8b52f640c880c8a56b93aaa54f7db8ee1fb
Bersserk <bers.developer@gmail.com>
2022-07-22 17:20:20 +0200

    Redone last three comments

--- app/src/main/java/com/example/first_project_fm/Anagram.java
+++ app/src/main/java/com/example/first_project_fm/Anagram.java
@@ -7,22 +7,19 @@ public class Anagram {
         StringBuilder output = new StringBuilder();
 
         // Making the anagram for each word
-        for (String word : input.split(" ")) {
-            output.append(buildAnagramOfWord(word, filter.isEmpty()?"default":filter));
+        for (String word : input.split("\\s")) {
+            output.append(buildAnagramOfWord(word, filter));
         }
-
         return output.toString();
     }
 
     private static String buildAnagramOfWord(String word, String filter) {
         StringBuilder newWord = new StringBuilder(word);
 
-        for (int i = 0, k = newWord.length() - 1; i < k; ) {
-            if (check(newWord.charAt(i), filter)) {
-                i++;
-            } else if (check(newWord.charAt(k), filter)) {
-                k--;
-            } else {
+        for (int i = 0, k = newWord.length() - 1; i < k; )
+            if (check(newWord.charAt(i), filter)) i++;
+            else if (check(newWord.charAt(k), filter)) k--;
+            else {
                 char tmp = newWord.charAt(k);
                 newWord.deleteCharAt(k);
                 newWord.insert(k, newWord.charAt(i));
@@ -31,16 +28,15 @@ public class Anagram {
                 i++;
                 k--;
             }
-        }
 
         return newWord + " ";
     }
 
     private static boolean check(char i, String filter) {
 
-        if (filter.equals("default"))
-            return Character.isAlphabetic(i) ? false : true;
-            return filter.indexOf(i) > -1 ? true : false;
+        if (filter.isEmpty())
+            return !Character.isAlphabetic(i);
+        return filter.indexOf(i) > -1
     }
 
 }
